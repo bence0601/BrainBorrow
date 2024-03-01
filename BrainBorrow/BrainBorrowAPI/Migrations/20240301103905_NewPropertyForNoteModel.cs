5d79a7e1a7278f43706e1321a052305184fcb63e
Bence Nagy <nagybence0601@gmail.com>
2024-03-01 11:41:51 +0100

    Add : New property for the NoteModel, previous was buggy

---
+++ BrainBorrow/BrainBorrowAPI/Migrations/20240301103905_NewPropertyForNoteModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrainBorrowAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPropertyForNoteModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "University",
+                table: "Notes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "University",
+                table: "Notes");
+        }
+    }
+}
