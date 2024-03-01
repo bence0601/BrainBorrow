fe3ea1034fe4b0e140e556b364b31ebaee572ae3
Bence Nagy <nagybence0601@gmail.com>
2024-03-01 11:27:26 +0100

    Add : NoteController with POST request, and INoteInterface

---
+++ BrainBorrow/BrainBorrowAPI/Controllers/NoteController.cs
@@ -0,0 +1,33 @@
+﻿using BrainBorrowAPI.Models;
+using BrainBorrowAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainBorrowAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NoteController : ControllerBase
+    {
+        public INoteService _NoteService;
+        public NoteController(INoteService notesService)
+        {
+            _NoteService = notesService;
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<List<NoteModel>>> Register(NoteModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _NoteService.CreateNoteAsync(model);
+            return Ok(result);
+        }
+
+
+    }
+}
+
---
+++ BrainBorrow/BrainBorrowAPI/Services/INoteService.cs
@@ -0,0 +1,9 @@
+﻿using BrainBorrowAPI.Models;
+
+namespace BrainBorrowAPI.Services
+{
+    public interface INoteService
+    {
+        public Task<List<NoteModel>> CreateNoteAsync(NoteModel model);
+    }
+}
