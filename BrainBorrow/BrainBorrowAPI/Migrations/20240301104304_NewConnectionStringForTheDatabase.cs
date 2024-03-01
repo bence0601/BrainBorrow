8680ca251327e62f459bb3c8de870b0d865b042e
Bence Nagy <nagybence0601@gmail.com>
2024-03-01 12:17:54 +0100

    Fix : Finally the api can send requests to the db in container, and i can use dotnet ef database commands as well

---
+++ BrainBorrow/BrainBorrowAPI/Migrations/20240301104304_NewConnectionStringForTheDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrainBorrowAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewConnectionStringForTheDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
